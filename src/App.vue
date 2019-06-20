97f41b97363651fdc7b2536a62c413f226629b8c
dafyddllyr <dafyddllyr@gmail.com>
2019-06-20 11:55:28 +0100

    API request complete and data modified for GCharts format

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
   <div id="app">
-
+    <h1>Energy Mix</h1>
+    <energy-chart />
   </div>
 </template>
 
 <script>
+import EnergyChart from '@/components/EnergyChart.vue'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      generationMix: null,
+      generationMixChartData: null
+    }
+  },
   components: {
+    'energy-chart': EnergyChart
+  },
+  mounted() {
+    this.fetchEnergyData()
+  },
+  methods: {
+    fetchEnergyData: function() {
+      fetch('https://api.carbonintensity.org.uk/generation')
+      .then(response => response.json())
+      .then(apiResult => {
+        const generationMix = apiResult.data.generationmix;
+        this.generationMix = generationMix
+        this.generationMixChartData =
+          generationMix.map(type => [type.fuel, type.perc]);
+        this.generationMixChartData.unshift(['Fuel', 'Percentage']);
+      }
+    )
+
+
   }
+}
 }
 </script>
 
---
+++ src/components/EnergyChart.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <GChart
+  type="ColumnChart"
+  :data="chartData"
+  :options="chartOptions"
+/>
+</template>
+
+<script>
+export default {
+  name: 'energy-chart'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
